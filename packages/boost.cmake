d713ddee745768b3b77cdc95fe2806ab2d5b8ae3
Andarwinux <Andarwinux@users.noreply.github.com>
2026-04-30 00:00:00 +0000

    boost: update to 1.91.0-1

--- packages/boost.cmake
+++ packages/boost.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(boost
-    URL https://github.com/boostorg/boost/releases/download/boost-1.90.0/boost-1.90.0-b2-nodocs.tar.xz
-    URL_HASH SHA256=9E6BEE9AB529FB2B0733049692D57D10A72202AF085E553539A05B4204211A6F
+    URL https://github.com/boostorg/boost/releases/download/boost-1.91.0-1/boost-1.91.0-1-b2-nodocs.tar.xz
+    URL_HASH SHA256=7334bb672b9c7aa135da88bcc3111a64a198be9a7d44eb5151d9248e7cfd43ef
     DOWNLOAD_DIR ${SOURCE_LOCATION}
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
